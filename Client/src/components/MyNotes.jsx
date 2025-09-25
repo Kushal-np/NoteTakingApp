89702f7ae7f87f51212b9c82ca828b8f8747db5e
Kushal-np <wrckushal@gmail.com>
2025-09-25 20:50:39 +0545

    Basic structure of My notes page

---
+++ Client/src/components/MyNotes.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const MyNotes = () => {
+  return (
+    <div>
+        <div>
+            {/* users name  */}
+            <div></div>
+            {/* sort by  */}
+            <div></div>
+        </div>
+        <div>
+            {/* Lists  */}
+            <div>
+                <div>
+                    <div>Title</div>
+                    <div>
+                        <div>view</div>
+                        <div>edit</div>
+                        <div>delete</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            Add new note ? <div>Create</div>
+        </div>
+    </div>
+  )
+}
+
+export default MyNotes
