5b10d890e0f6d5a0da9eb01d8e5ca52e656d49b8
Kushal-np <wrckushal@gmail.com>
2025-09-25 20:57:16 +0545

    Done with the remove page

---
+++ Client/src/components/Remove.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Remove = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                Delete your Notes
+            </div>
+            <div>
+                <div>
+                    List of all your Notes 
+                </div>
+                <div>
+                    Sort
+                </div>
+            </div>
+            <div>
+                <div>
+                    <input type="checkbox" />
+                </div>
+                <div>
+                    title
+                </div>
+            </div>
+            <div>
+                <button>
+                    Delete All
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Remove
