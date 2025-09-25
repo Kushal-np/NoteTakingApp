38e891601d4b231959409e60f6954188748ffd6d
Kushal-np <wrckushal@gmail.com>
2025-09-25 20:47:54 +0545

    Basic strucutre of the edit and controls page

---
+++ Client/src/components/EditAndControls.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import SideBar from './SideBar'
+
+const MyNotes = () => {
+  return (
+    <div>
+        <div>
+            <SideBar/>
+        </div>
+
+        <div>
+            <div>
+                {/* The edits and controls section  */}
+                <div> </div>
+                {/* List and notes section  */}
+                <div>
+                    {/* title of the section  */}
+                    <div>
+
+                    </div>
+                    {/* actual lists will be shown here  */}
+                    <div>
+
+                    </div>
+                    {/* see more button  */}
+                    <div>
+                        <button>Submit</button>
+                    </div>
+                </div>
+                {/* actual editing section  */}
+                <div>
+                    <div>
+                        {/* Enter title section  */}
+                        <div></div>
+                        {/* text editor  */}
+                        <div></div>
+                        {/* tags section  */}
+                        <div></div>
+                        <div>
+                            <button>
+                                Update
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default MyNotes
